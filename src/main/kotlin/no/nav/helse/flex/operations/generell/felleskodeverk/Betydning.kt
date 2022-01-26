7bb6fbe27c3ad30ef2070c32ae7f46275bbaff28
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-01-26 17:15:47 +0100

    Er nullable, fjernes siden de ikke brukes i koden

--- src/main/kotlin/no/nav/helse/flex/operations/generell/felleskodeverk/Betydning.kt
+++ src/main/kotlin/no/nav/helse/flex/operations/generell/felleskodeverk/Betydning.kt
@@ -1,8 +1,6 @@
 package no.nav.helse.flex.operations.generell.felleskodeverk
 
 data class Betydning(
-    var gyldigFra: String,
-    var gyldigTil: String,
     var beskrivelser: Map<String, Beskrivelse>
 ) {
     fun init(): TemaSkjemaData {
