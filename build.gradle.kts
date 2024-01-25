0cefb165bee058dd5dd0a51b7ee0eb43355c3d5c
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-25 09:15:15 +0100

    JVM 21 Target (#189)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -12,7 +13,7 @@ plugins {
 group = "no.nav.helse.flex"
 version = "1.0.0"
 description = "flex-joark-mottak"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
@@ -80,11 +81,12 @@ tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+        freeCompilerArgs.add("-Xjsr305=strict")
+
         if (System.getenv("CI") == "true") {
-            kotlinOptions.allWarningsAsErrors = true
+            allWarningsAsErrors.set(true)
         }
     }
 }
